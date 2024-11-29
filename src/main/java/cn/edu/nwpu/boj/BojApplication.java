4bb492bf0d847144c3e6bf9ed122dd8d04544438
haomingbai <haomingbai@hotmail.com>
2024-11-30 00:48:42 +0800

    位于分支 master
     初始提交
    
     要提交的变更：
    	新文件：   .gitattributes
    	新文件：   .gitignore
    	新文件：   build.gradle.kts
    	新文件：   gradle/wrapper/gradle-wrapper.jar
    	新文件：   gradle/wrapper/gradle-wrapper.properties
    	新文件：   gradlew
    	新文件：   gradlew.bat
    	新文件：   settings.gradle.kts
    	新文件：   src/main/java/cn/edu/nwpu/boj/BojApplication.java
    	新文件：   src/main/resources/application.properties
    	新文件：   src/test/java/cn/edu/nwpu/boj/BojApplicationTests.java

---
+++ src/main/java/cn/edu/nwpu/boj/BojApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nwpu.boj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/nwpu/boj/BojApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nwpu.boj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
